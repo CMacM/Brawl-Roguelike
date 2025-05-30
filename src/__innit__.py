fdf1a2734c9ae43631ddfcd6c5aa8087eb7626f6
Charlie MacMahon <macmahon14.02@gmail.com>
2025-05-31 00:10:49 +0100

    update version

--- src/__innit__.py
+++ src/__innit__.py
@@ -3,5 +3,5 @@ from data import round_pools
 from utilities import roll_champions
 
 __all__ = ["fetch_champion_assets", "round_pools", "roll_champions"]
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __authors__ = "Brawl Rogue Team"
